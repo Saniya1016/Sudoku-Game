14c1362c874aab58f9b6dd15f30e78726e26134c
saniya <saniyasingh@gmail.com>
2024-09-12 17:05:09 -0400

    solve entire functional

--- my-sudoku-game/src/Components/Solve.jsx
+++ my-sudoku-game/src/Components/Solve.jsx
@@ -4,8 +4,8 @@ import solve from '../utils/Solver';
 
 const Solve = ({gameState, setGameState}) => {
 
-    const handleOneStep = () => {
-        const newState = solve({ ...gameState }, false);
+    const handleOneStep = (flag) => {
+        const newState = solve({ ...gameState }, flag);
         setGameState(newState);
     }
 
@@ -13,8 +13,8 @@ const Solve = ({gameState, setGameState}) => {
     <div>
 
         <div className='flex flex-row space-x-10'>
-            <button className='text-white bg-yellow-300 hover:bg-yellow-200 px-4 py-2 rounded-lg' onClick={handleOneStep}> Reveal One Step </button>
-            <button className='text-white bg-green-500 hover:bg-green-300 px-4 py-2 rounded-lg'> See Solution </button>
+            <button className='text-white bg-yellow-300 hover:bg-yellow-200 px-4 py-2 rounded-lg' onClick={() => {handleOneStep(false)}}> Reveal One Step </button>
+            <button className='text-white bg-green-500 hover:bg-green-300 px-4 py-2 rounded-lg' onClick={() => {handleOneStep(true)}}> See Solution </button>
         </div>
       
     </div>
